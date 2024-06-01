386f8b14b21b0604034f29d02b881c430175df73
PixepChief <impsotorissus91898@gmail.com>
2024-06-01 15:15:54 +0100

    few bugs related to charids and index

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const charIcons = document.querySelectorAll(".character-icon");
-  const currentChar = document.getElementById("currentChar");
+    const charIcons = document.querySelectorAll(".character-icon");
+    const currentChar = document.getElementById("currentChar");
 
-  fetch('charids.json')
-    .then(response => response.json())
-      .then(charIds => {
-          charIcons.forEach(charIcon => {
-              charIcon.addEventListener('click', function () {
-                  const charName = charIcon.getAttribute('data-card2');
-                  if (charIds[charName]) {
-                      const charId = charIds[charName];
-                      currentChar.src = `./characters/${charName}/${charId}_00_00.png`;
-                  } else {
-                      console.error(`Character name "${charName}" not found in charids.json`);
-                  }
-              });
-          });
-      })
-      .catch(error => console.error('Error loading charids.json:', error));
+    function imageExists(imageUrl, callback) {
+        const img = new Image();
+        img.onload = function () {
+            callback(true);
+        };
+        img.onerror = function () {
+            callback(false);
+        };
+        img.src = imageUrl;
+    }
+
+    fetch("charids.json")
+        .then((response) => response.json())
+        .then((charIds) => {
+            charIcons.forEach((charIcon) => {
+                charIcon.addEventListener("click", function () {
+                    const charName = charIcon.getAttribute("data-card2");
+                    if (charIds[charName]) {
+                        const charId = charIds[charName];
+                        const firstImagePath = `./characters/${charName}/${charId}_00_00.png`;
+                        const fallbackImagePath = `./characters/${charName}/${charId}_00_00_00.png`;
+
+                        imageExists(firstImagePath, function (exists) {
+                            if (exists) {
+                                currentChar.src = firstImagePath;
+                            } else {
+                                currentChar.src = fallbackImagePath;
+                            }
+                        });
+                    } else {
+                        console.error(
+                            `Character name "${charName}" not found in charids.json`
+                        );
+                    }
+                });
+            });
+        })
+        .catch((error) => console.error("Error loading charids.json:", error));
 });
